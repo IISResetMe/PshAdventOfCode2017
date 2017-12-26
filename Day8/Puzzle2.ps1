2fbdcfaa7d3161737a5ae6d559f063e37e7da86f
Mathias R. Jessen <IISResetMe@users.noreply.github.com>
2017-12-26 15:15:55 +0100

    Create Puzzle2.ps1

---
+++ Day8/Puzzle2.ps1
@@ -0,0 +1,44 @@
+function Execute-Instructions
+{
+    param(
+        [string]$Path = 'input.txt'
+    )
+
+    $Instructions = Get-Content $Path
+    $Registers = @{}
+
+    $highest = 0
+
+    $Instructions |ForEach-Object {
+        $m = $($_ |sls '^(?<reg>\w+) (?<ins>(in|de)c) (?<delta>\-?\d+) if (?<sub>\w+) (?<op>[<>!=]*) (?<test>\-?\d+)$').Matches[0]
+        $eval = switch($m.Groups['op'].Value){
+            '>' {
+                {param($a,$b) $a -gt $b}
+            }
+            '<' {
+                {param($a,$b) $a -lt $b}
+            }
+            '>=' {
+                {param($a,$b) $a -ge $b}
+            }
+            '<=' {
+                {param($a,$b) $a -le $b}
+            }
+            '==' {
+                {param($a,$b) $a -eq $b}
+            }
+            '!=' {
+                {param($a,$b) $a -ne $b}
+            }
+        }
+        $a,$b = +$Registers[$m.Groups['sub'].Value],+$m.Groups['test'].Value
+        if(&$eval -a $a -b $b){
+            $delta = (@(1,-1)[$m.Groups['ins'].Value -eq 'dec'] * $m.Groups['delta'].Value)
+            $highest = [System.Math]::Max($highest,($Registers[$m.Groups['reg'].Value] += $delta))
+        }
+    }
+
+    return $highest
+}
+
+Execute-Instructions
